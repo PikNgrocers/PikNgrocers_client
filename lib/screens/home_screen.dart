dcdea131106017df6f1e12c756236b2c525e67b6
Kadirvelan <kadirvelanlinkes@gmail.com>
2020-10-03 18:00:18 +0530

    Shimmer products loading page done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,38 +1,161 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pikngrocers_client/constants.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.location_searching,size: 15.0,),
-                        label: Text('Main Address',style: TextStyle(fontSize: 15),),
-                      ),
-                      Text('Address'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      body: Center(
+          child: DelayedList()),
+    );
+  }
+}
+
+class DelayedList extends StatefulWidget {
+  @override
+  _DelayedListState createState() => _DelayedListState();
+}
+
+class _DelayedListState extends State<DelayedList> {
+  bool isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    Timer timer = Timer(Duration(seconds: 3), () {
+      setState(() {
+        isLoading = true;
+      });
+    });
+
+    return isLoading ? ShimmerList() : DataList(timer);//This is for now only... if is loading is false we have to load the cards
+  }
+}
+
+class DataList extends StatelessWidget {
+  final Timer timer;
+
+  DataList(this.timer);
+
+  @override
+  Widget build(BuildContext context) {
+    timer.cancel();
+    return ListView.builder(
+      itemCount: 8,
+      itemBuilder: (context, i) {
+        return Container(
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Text(i.toString()),
+        );
+      },
+    );
+  }
+}
+
+class ShimmerList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int offset = 0;
+    int time = 800;
+
+    return SafeArea(
+      child: ListView.builder(
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          offset += 5;
+          time = 800 + offset;
+
+          print(time);
+
+          return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Shimmer.fromColors(
+                highlightColor: Colors.white,
+                baseColor: Colors.deepPurple[200],
+                child: ShimmerLayout(),
+                period: Duration(milliseconds: time),
+              ));
+        },
       ),
     );
   }
 }
+
+class ShimmerLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double containerWidth = 260;
+    double containerHeight = 15;
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 7.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.grey,
+          ),
+          Column(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 5),
+              Container(
+                height: containerHeight,
+                width: containerWidth * 0.35,
+                color: Colors.grey,
+              ),
+              SizedBox(height: 5),
+              Container(
+                height: containerHeight,
+                width: containerWidth*0.70,
+                color: Colors.grey,
+              ),
+              SizedBox(height: 5),
+              Container(
+                height: containerHeight,
+                width: containerWidth*0.70,
+                color: Colors.grey,
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  Container(
+                    height: containerHeight,
+                    width: containerWidth*0.70,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width:60),
+                  Container(
+                    height: 18,
+                    width: 30,
+                    color: Colors.grey,
+                  )
+                ],
+              ),
+              SizedBox(height: 5),
+
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
