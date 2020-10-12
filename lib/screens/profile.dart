3265e4801caaccaf1a4fe7c36bcf8a873ae68394
kani kapoor <kanikapoor24@gmail.com>
2020-10-12 14:56:05 +0530

    completed client side

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -40,7 +40,9 @@ class ProfilePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Text('ORDERS'),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     StreamBuilder<QuerySnapshot>(
                         stream: Database().orderDetails(userId: userId),
                         builder: (context, snapshot) {
@@ -54,65 +56,65 @@ class ProfilePage extends StatelessWidget {
                                   itemCount: doc.length,
                                   shrinkWrap: true,
                                   itemBuilder: (context, i) {
-                                    return Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                      child: ListTile(
-                                        tileColor: doc[i].data()['OrderStatus'] ==
-                                                'Waiting'
-                                            ? Colors.deepOrange
-                                            : doc[i].data()['OrderStatus'] ==
-                                                    'Accepted'
-                                                ? Colors.blue
-                                                : Colors.green,
-                                        title: doc[i].data()['OrderStatus'] ==
-                                                'Waiting'
-                                            ? Text(
-                                                'Order waiting',
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              )
-                                            : doc[i].data()['OrderStatus'] ==
-                                                    'Accepted'
-                                                ? Text(
-                                                    'Order Accepted',
-                                                    style: TextStyle(
-                                                        color: Colors.white),
-                                                  )
-                                                : Text(
-                                                    'Ready to Pick',
-                                                    style: TextStyle(
-                                                        color: Colors.white),
-                                                  ),
-                                        subtitle: doc[i].data()['OrderStatus'] ==
-                                                'Waiting'
-                                            ? Text(
-                                                'please wait for vendor to accept your order',
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              )
-                                            : doc[i].data()['OrderStatus'] ==
-                                                    'Accepted'
-                                                ? Text(
-                                                    'Now your products are Packing',
-                                                    style: TextStyle(
-                                                        color: Colors.white),
-                                                  )
-                                                : Text(
-                                                    'Collect your order by visiting the store',
-                                                    style: TextStyle(
-                                                        color: Colors.white),
-                                                  ),
-                                        trailing: doc[i].data()['OrderStatus'] ==
-                                            'Waiting'
-                                            ? Icon(Icons.timer,color: Colors.white,)
-                                            : doc[i].data()['OrderStatus'] ==
-                                            'Accepted'
-                                            ? Icon(Icons.check_box_outline_blank_sharp,color: Colors.white,)
-                                            : Icon(Icons.done,color: Colors.white,),
-                                      ),
-                                    );
+                                    if (doc[i].data()['OrderStatus'] ==
+                                        'Waiting') {
+                                      return OrderListTileWidget(
+                                        tileColor: Colors.orange,
+                                        title: 'Order Waiting',
+                                        textColor: Colors.white,
+                                        subTitle:
+                                            'please wait for vendor to accept your order',
+                                        icon: Icon(
+                                          Icons.timer,
+                                          color: Colors.white,
+                                        ),
+                                      );
+                                    }
+                                    if (doc[i].data()['OrderStatus'] ==
+                                        'Accepted') {
+                                      return OrderListTileWidget(
+                                        tileColor: Colors.blue,
+                                        title: 'Order Accepted',
+                                        textColor: Colors.white,
+                                        subTitle:
+                                        'Now your products are Packing',
+                                        icon: Icon(
+                                          Icons
+                                              .check_box_outline_blank_sharp,
+                                          color: Colors.white,
+                                        )
+                                      );
+                                    }
+                                    if (doc[i].data()['OrderStatus'] ==
+                                        'Packed') {
+                                      return OrderListTileWidget(
+                                          tileColor: Colors.green,
+                                          title: 'Ready to Pick',
+                                          textColor: Colors.white,
+                                          subTitle:
+                                          'Collect your order by visiting the store',
+                                          icon: Icon(
+                                            Icons
+                                                .directions_walk,
+                                            color: Colors.white,
+                                          )
+                                      );
+                                    }
+                                    if (doc[i].data()['OrderStatus'] ==
+                                        'Cash Received') {
+                                      return OrderListTileWidget(
+                                          tileColor: Colors.white,
+                                          title: 'Order Completed',
+                                          textColor: Colors.green,
+                                          subTitle: null,
+                                          icon: Icon(
+                                            Icons
+                                                .done,
+                                            color: Colors.green,
+                                          )
+                                      );
+                                    }
+                                    return Center(child: Text('No Orders'),);
                                   });
                             }
                           }
@@ -124,6 +126,39 @@ class ProfilePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class OrderListTileWidget extends StatelessWidget {
+  final Color tileColor;
+  final String title;
+  final Color textColor;
+  final String subTitle;
+  final Icon icon;
+
+  OrderListTileWidget(
+      {this.title, this.icon, this.subTitle, this.textColor, this.tileColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: ListTile(
+        tileColor: tileColor,
+        title: Text(
+          '$title',
+          style: TextStyle(color: textColor),
+        ),
+        subtitle: subTitle !=null ? Text(
+          '$subTitle',
+          style: TextStyle(color: textColor),
+        ) : null,
+        trailing: icon,
       ),
     );
   }
